1fbe401c0b37d91329cb4aa9d60a915720b5aac2
Anujksh5 <2003010036@ipec.org.in>
2024-05-19 14:12:16 +0530

    Updated

---
+++ Instant_News.py
@@ -0,0 +1,31 @@
+import sys
+def speak(str):
+    from win32com.client import Dispatch 
+    speak = Dispatch("SAPI.SpVoice")
+    speak.Speak(str)
+
+try:
+    if __name__ == '__main__':
+        import requests
+        import json
+        url = ('http://newsapi.org/v2/top-headlines?'
+            'country=in&'
+            'apiKey=f4c7466fc5ea463cbb0f1a69941c1f3a')
+        response = requests.get(url)
+        text = response.text
+        jscomp = json.loads(text)
+        print("_______________Indian News________________")
+        speak("Some Indian News headlines")
+        for i in range(10):
+            print("News " + str(i+1)+ " : " + jscomp['articles'][i]['title'])
+            speak("News " + str(i+1)+ " : " + jscomp['articles'][i]['title'])
+
+except KeyboardInterrupt:
+    sys.exit()
+
+
+
+
+
+
+
